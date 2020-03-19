37e18a49bd7deafa470183ba8deb8bc09375a394
Vlad Muravyov <vlad.muravyov@mail.ru>
2020-03-20 00:28:24 +0500

    css first mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 import "./client.js";
 import "./controller.js";
 import "./chatView.js";
