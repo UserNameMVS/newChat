d547ee7a7cdfb3faf92b7288fb6d961a3e418e64
Vlad Muravyov <vlad.muravyov@mail.ru>
2020-03-31 00:46:56 +0500

    add apiRequest

--- js/apiClient.js
+++ js/apiClient.js
@@ -2,11 +2,54 @@
 
 import { serverURL } from "./config.js";
 
-// function apiRequest(url, method, params) {
-//   return new Promise((resolve, reject) => {
-//     let dataFromServer;
-//     resolve(dataFromServer);
-//   })
-// }
+// export const apiRequest = (apiPath, params, config) => {
+//   return new Promise(resolve => {
+//     let res = fetch(serverURL + apiPath + params, config);
+//     resolve(res)
+//   }).then(res => console.log(res.json()))
+//     .catch(err => console.error("Ошибка: ", err));
+// };
 
-// promise.then(console.log);
+export const apiRequest = async (apiPath, params, config) => {
+  try {
+    let res = await fetch(serverURL + apiPath + params, config);
+    let data = res.json();
+    console.log(data); //return data
+  } catch (err) {
+    console.error("Ошибка: ", err);
+  }
+};
+
+apiRequest("/api/user?", "username=Jonh", { method: "GET" });
+
+export const getUser = userName => {
+  let params = `username=${userName}`;
+  const apiPath = "/api/user?";
+  const config = {
+    method: "GET"
+  };
+  return apiRequest(apiPath, params, config);
+};
+
+getUser("John");
+
+export const createUser = (userName, userPassword) => {
+  let params = `username=${userName}`;
+  const apiPath = "/api/user?";
+  const payload = {
+    username: userName,
+    password: userPassword
+  }
+  const config = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(payload)
+  };
+  return apiRequest(apiPath, params, config);
+};
+
+// createUser("vlad", "ant555");
+
