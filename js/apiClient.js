fd78057e3df10e426643b6e1379b66c35232595d
Vlad Muravyov <vlad.muravyov@mail.ru>
2020-04-15 19:30:55 +0500

    add auth user

--- js/apiClient.js
+++ js/apiClient.js
@@ -1,54 +1,94 @@
 "use strict";
 
 import { serverURL } from "./config.js";
-import { inputLogin, inputPassword } from "./uiElements.js";
+import {
+  inputLoginAccount,
+  inputPasswordAccount,
+  logOut,
+  authPage,
+  linkToAccount,
+  accountPage,
+  linkToAuth,
+  btnCreateAccount,
+  inputLoginAuth,
+  inputPasswordAuth,
+  authButton,
+} from "./uiElements.js";
 
 export const apiRequest = async (apiPath, config, params) => {
   try {
     let res = await fetch(serverURL + apiPath + params, config);
     let data = await res.json();
+    if (res.status == 400) {
+      throw new SyntaxError(data.error);
+    }
     return data;
   } catch (err) {
-    console.error("Ошибка: ", err);
+    console.log("Ошибка: ", err.message);
   }
 };
 
-export const getUser = async (userName) => {
-  let params = `username=${userName}`;
+export const getUser = async (username) => {
   const apiPath = "/api/user?";
   const config = {
     method: "GET",
   };
-  console.log(await apiRequest(apiPath, config, params));
+  let params = `username=${username}`;
+  apiRequest(apiPath, config, params).then((data) =>
+    data.user.username
+  );
 };
 
-// getUser("vlad");
+function addUser() {
+  let loginUser = inputLoginAccount.value;
+  let passwordUser = inputPasswordAccount.value;
+  checkValidLogin(loginUser);
+  checkValidPassword(passwordUser);
+  if (checkValidLogin && checkValidPassword) {
+    loginUser = checkValidLogin(loginUser);
+    passwordUser = checkValidPassword(passwordUser);
+    createUser(loginUser, passwordUser);
+  }
+}
 
-export const createUser = async (userName, userPassword) => {
-  let params = `username=${userName}`;
+btnCreateAccount.addEventListener("click", addUser);
+
+export const createUser = async (username, password) => {
+  let params = `username=${username}`;
   const apiPath = "/api/user?";
   const payload = {
-    username: userName,
-    password: userPassword,
+    username,
+    password,
   };
   const config = {
     method: "POST",
     headers: {
-      "Accept": "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify(payload),
   };
-  console.log(await apiRequest(apiPath, config, params));
+  apiRequest(apiPath, config, params);
 };
 
-// createUser("testUser", "ant555");
+function authUser() {
+  let username = inputLoginAuth.value;
+  let password = inputPasswordAuth.value;
+  authLoginAndPassword(username, password)
+  .then(() => {
+    if(getCookie("token") && getCookie("token") !== "undefined") {
+      authPage.classList.add("hide");
+    }
+  });
+}
 
-export const authLoginAndPassword = async (userName, userPassword) => {
+authButton.addEventListener("click", authUser);
+
+export const authLoginAndPassword = async (username, password) => {
   const apiPath = "/api/user/auth?";
   const payload = {
-    username: userName,
-    password: userPassword,
+    username,
+    password,
   };
   const config = {
     method: "POST",
@@ -56,26 +96,73 @@ export const authLoginAndPassword = async (userName, userPassword) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(payload),
-  }
-  let data = await apiRequest(apiPath, config);
-  const token = data.token;
-  console.log(token) //return token
-}
+  };
+  apiRequest(apiPath, config).then((data) => {
+    setCookie("token", data.token, { secure: true });
+  });
+};
 
-authLoginAndPassword("testUser", "ant555")
+logOut.addEventListener("click", function () {
+  deleteCookie("token");
+  authPage.classList.remove("hide");
+});
 
-function checkValidationLogin() {
-  let loginUser = inputLogin.value;
+linkToAccount.addEventListener("click", function () {
+  authPage.classList.toggle("hide");
+  accountPage.classList.toggle("hide");
+});
+
+linkToAuth.addEventListener("click", function () {
+  accountPage.classList.toggle("hide");
+  authPage.classList.toggle("hide");
+});
+
+function checkValidLogin(loginUser) {
   loginUser = loginUser.trim();
-  if (loginUser.length < 2) {
-    console.log("Длина имени пользователя должна быть не менее 2-х символов");
+  if (loginUser.length >= 2) {
+    return loginUser;
   }
 }
 
-function checkValidationLPassword() {
-  let passwordUser = inputPassword.value;
+function checkValidPassword(passwordUser) {
   passwordUser = passwordUser.trim();
-  if (passwordUser.length < 4) {
-    console.log("Длина пароля должна быть не менее 4-х символов");
+  if (passwordUser.length >= 4) {
+    return passwordUser;
   }
+}
+
+function setCookie(name, value, options = {}) {
+  options = {
+    path: "/",
+  };
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+  let updatedCookie =
+    encodeURIComponent(name) + "=" + encodeURIComponent(value);
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+  document.cookie = updatedCookie;
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function deleteCookie(name) {
+  setCookie(name, "", {
+    "max-age": -1,
+  });
 }
